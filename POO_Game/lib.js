01fbaf407c225dc24dca867caf7007cfb9e4ff0c
Ggabriel-SS <97918580+Ggabriel-SS@users.noreply.github.com>
2022-11-07 14:21:57 -0300

    Update lib.js

--- POO_Game/lib.js
+++ POO_Game/lib.js
@@ -1,12 +1,13 @@
 let canvas = document.getElementById('canvas')
 let ctx = canvas.getContext('2d')
 class Player{
-    constructor(pontox, pontoy, largura, altura, img){
+    constructor(pontox, pontoy, largura, altura,vel, img){
         this.x = pontox,
         this.y = pontoy,
         this.altura = altura,
         this.largura = largura,
         this.vida = 3,
+        this.velocidade = vel,
         this.img = new Image(),
         this.img.src = img
     }
@@ -20,11 +21,21 @@ class Player{
         ctx.drawImage(this.img,this.x,this.y,this.largura,this.altura)
     }
     cair(){
-        this.y += 5
+        this.y += this.velocidade
     }
     voltar(){
         if(this.y >= 782){
             this.y = 0
+        }
+    }
+    moverDireita(){
+        if(this.x + this.largura < 500){
+            this.x += 100
+        }
+    }
+    moverEsquerda(){
+        if(this.x > 0){
+            this.x -= 100
         }
     }
 }
